d4323be6f663cda3c2b2a021f479ac960bbc62c6
EnerL77 <post@renelamberti.de>
2025-10-30 11:03:19 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        roseAccent: "#D8A5A9",
+        dark: "#0a0a0a"
+      }
+    }
+  },
+  plugins: []
+};
