dfb223000b0193f2399d9a8adf5bee7af3b9569d
EnerL77 <post@renelamberti.de>
2025-10-30 11:22:08 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import Page from "./App.jsx";   // ← App → Page
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Page />                    // ← <App /> → <Page />
   </React.StrictMode>
 );
